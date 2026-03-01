583595e656caed51e1c1c48eb11b2183c2ef93a0
falihMS <falih72ms@gmail.com>
2026-03-01 13:11:43 +0700

    add temporary bot transaction

--- components/form/bot-transaction.tsx
+++ components/form/bot-transaction.tsx
@@ -1,10 +1,13 @@
 "use client"
 
-import { FieldGroup } from "../ui/field"
+import { Field, FieldGroup, FieldLabel } from "../ui/field"
 import { revalidateLogic, StandardSchemaV1Issue } from "@tanstack/react-form"
 import { toast } from "sonner"
 import z from "zod"
 import { useAppForm } from "@/hooks/form"
+import { InputGroup, InputGroupAddon, InputGroupButton, InputGroupTextarea } from "../ui/input-group"
+import { LoaderCircle, MicIcon, PaperclipIcon, SendIcon } from "lucide-react"
+import { DevelopmentDialog } from "../dialog/development-dialog"
 
 const formSchema = z.object({
     chat: z.string().min(1, "Explain the transaction"),
@@ -33,9 +36,6 @@ export default function BotTransactionForm() {
             })
         },
         onSubmitInvalid: ({ formApi }) => {
-            console.log(formApi.state.errors)
-            console.log(formApi.state.isValid)
-
             const errors = formApi.state.errors[0]
             if (!errors) return
 
@@ -60,21 +60,74 @@ export default function BotTransactionForm() {
             <div className="w-full mt-2 px-4 md:px-8">
                 <form
                     className="grid lg:grid-cols-2"
-                    id="transaction-form"
+                    id="bot-form"
                     onSubmit={(e) => {
                         e.preventDefault()
                         form.handleSubmit()
                     }}
                 >
                     <FieldGroup className="mb-5 gap-4">
-                        
-                        <form.AppField
+
+                        <form.Field
                             name="chat"
-                            children={(field) => <field.TextareaField label="Record Your Transaction" />}
+                            children={(field) => {
+                                return (
+                                    <Field>
+                                        <FieldLabel htmlFor={field.name}>Transaction Detail</FieldLabel>
+                                        <InputGroup>
+                                            <InputGroupTextarea
+                                                id={field.name}
+                                                name={field.name}
+                                                value={field.state.value}
+                                                onBlur={field.handleBlur}
+                                                onChange={(e) => field.handleChange(e.target.value)}
+                                                placeholder="Optional notes"
+                                                rows={2}
+                                                className="min-h-20 resize-none"
+                                                aria-disabled={false}
+                                            />
+                                            <InputGroupAddon align="block-end" className="border-t flex justify-between">
+                                                <DevelopmentDialog>
+                                                    <InputGroupButton size="sm">
+                                                        <PaperclipIcon />
+                                                    </InputGroupButton>
+                                                </DevelopmentDialog>
+                                                 <form.Subscribe
+                                                    selector={(s) => ({
+                                                        chat: s.values.chat,
+                                                        isSubmitting: s.isSubmitting,
+                                                    })}
+                                                >
+                                                    {({ chat, isSubmitting }) => {
+                                                        if (chat === "") return (
+                                                            <DevelopmentDialog>
+                                                                <InputGroupButton disabled={isSubmitting} variant={"default"} size="sm">
+                                                                    <MicIcon />
+                                                                </InputGroupButton>
+                                                            </DevelopmentDialog>
+                                                        )
+
+                                                        return(
+                                                            <InputGroupButton disabled={isSubmitting} variant={"default"} size="sm" type="submit" form={"bot-form"}>
+                                                                {
+                                                                    !isSubmitting ?  <SendIcon /> : <LoaderCircle className="animate-spin" />
+                                                                }
+                                                            </InputGroupButton>
+                                                        )
+                                                    }}
+                                                </form.Subscribe>
+
+                                            </InputGroupAddon>
+
+                                        </InputGroup>
+                                    </Field>
+                                )
+                            }
+
+                            }
                         />
                     </FieldGroup>
                 </form>
-                <form.SubmitButtonField label="Save" />
             </div>
         </form.AppForm>
     )
