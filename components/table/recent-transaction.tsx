fd3ae46093b88d85b48cc009baad3bf70f1b934f
falihMS <falih72ms@gmail.com>
2026-03-03 15:16:09 +0700

    refator if transaction empty

--- components/table/recent-transaction.tsx
+++ components/table/recent-transaction.tsx
@@ -26,35 +26,45 @@ export function RecentTransactionTable({
         <Card className="lg:col-span-2">
             <CardHeader>
                 <CardTitle>Recent Transaction</CardTitle>
-                <CardAction>
-                    <DevelopmentDialog>
-                        <Button variant={"outline"}>View All</Button>
-                    </DevelopmentDialog>
-                </CardAction>
+                { data.length > 0 && (
+                    <CardAction>
+                        <DevelopmentDialog>
+                            <Button variant={"outline"}>View All</Button>
+                        </DevelopmentDialog>
+                    </CardAction>
+                ) }
             </CardHeader>
             <CardContent>
-                <Table>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead className="w-[75px]">Date</TableHead>
-                            <TableHead>Method</TableHead>
-                            <TableHead className="text-right">Amount</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {data.map((transaction, id) => (
-                            <TableRow key={id}>
-                                <TableCell className="font-medium">{transaction.transaction_date}</TableCell>
-                                <TableCell>{transaction.category} <span className="hidden md:inline-block font-light text-muted-foreground"> - {transaction.note}</span></TableCell>
-                                <TableCell className="text-right">
-                                    {
-                                        transaction.type === 'income' ? <span className="text-green-500">IDR {formatCurrencyDisplay(transaction.amount)}</span> : <span className="text-red-500">IDR {formatCurrencyDisplay(transaction.amount)}</span>
-                                    }
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
+                {
+                    data.length === 0 ? (
+                        <p className="text-muted-foreground">
+                            No Transaction Created Yet
+                        </p>
+                    ) : (
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[75px]">Date</TableHead>
+                                    <TableHead>Method</TableHead>
+                                    <TableHead className="text-right">Amount</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {data.map((transaction, id) => (
+                                    <TableRow key={id}>
+                                        <TableCell className="font-medium">{transaction.transaction_date}</TableCell>
+                                        <TableCell>{transaction.category} <span className="hidden md:inline-block font-light text-muted-foreground"> - {transaction.note}</span></TableCell>
+                                        <TableCell className="text-right">
+                                            {
+                                                transaction.type === 'income' ? <span className="text-green-500">IDR {formatCurrencyDisplay(transaction.amount)}</span> : <span className="text-red-500">IDR {formatCurrencyDisplay(transaction.amount)}</span>
+                                            }
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    )
+                }
             </CardContent>
         </Card>
     )
