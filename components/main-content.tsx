7fec0bd9d50ac720e86e938f51d52a97cd0b887f
falihMS <falih72ms@gmail.com>
2026-03-12 14:10:26 +0700

    adjust margin top layout

--- components/main-content.tsx
+++ components/main-content.tsx
@@ -14,7 +14,7 @@ export function MainContent({
                 <h3 className="font-medium">{title}</h3>
                 <SidebarTrigger className="invisible p-4" />
             </nav>
-            <main className="w-full max-w-3xl p-4 lg:mt-4 mx-auto grid grid-cols-1 gap-4">
+            <main className="w-full max-w-3xl p-4 md:mt-4 mx-auto grid grid-cols-1 gap-4">
                 <h3 className="hidden md:block font-medium text-2xl mb-2">{title}</h3>
                 {children}
             </main>
